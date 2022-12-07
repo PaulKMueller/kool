edf22998ee825cfdc0531e77f174467715b378dc
jasminazarfar <azarfarjasmin@gmail.com>
2022-12-07 23:39:21 +0000

    [refactor] unten Zeugs und popular comp geaddet

--- kool/landingpage/views.py
+++ kool/landingpage/views.py
@@ -7,7 +7,7 @@ from . import models
 
 content = []
 
-csvreader = pd.read_csv('/workspaces/kool/firsttest.csv', encoding='UTF-8', header=None)
+csvreader = pd.read_csv('/workspaces/kool/kool/firsttest.csv', encoding='UTF-8', header=None)
 
 #iterating through csv file using pandas
 for expert, trustworthiness in zip(csvreader[0], csvreader[1]):
