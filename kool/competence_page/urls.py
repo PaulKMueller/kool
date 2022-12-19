cbb68b9707a12768b09292b1424fbab0640ccde1
uuuqa <uuuqa@student.kit.edu>
2022-12-19 10:50:04 +0000

    [feature] clean up

--- kool/competence_page/urls.py
+++ kool/competence_page/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.categories_page),
-    path('biology', views.competence_page),
-    path('chemistry', views.competence_page),
-    path('cs', views.competence_page),
-    path('economics', views.competence_page),
     path('<int:id>', views.competence_page)
 ]
--- kool/competence_page/views.py
+++ kool/competence_page/views.py
@@ -8,8 +8,9 @@ biology = Category()
 biology.name = 'Biology'
 biology.link = '/competence/123'
 biology.img = 'https://cdn.pixabay.com/photo/2019/03/19/19/54/butterflies-4066785_960_720.jpg'
-main_url = "http://192.168.0.164:8020"
-url = "http://192.168.0.164:8020/all_categories"
+main_url = "http://192.168.123.116:8020"
+url = "http://192.168.123.116:8020/all_categories"
+
 
 def categories_page(request):
     category_names = get_request_from_api(url)
@@ -22,8 +23,6 @@ def categories_page(request):
         newCat.link = categorie[0]
         newCat.img = "https://picsum.photos/200/100"
         categories_obj.append(newCat)
-
-
 
     return render(request, 'competence_categories.html',
                   {'categories': categories_obj})
@@ -39,9 +38,10 @@ def get_request_from_api(url):
             continue
     return "connection failed"
 
+
 def competence_page(request, id):
     req_url = main_url + "/competencies_by_id/" + str(id)
     competencies = get_request_from_api(req_url)
     competencies.sort(key=lambda a: a[1])
     return render(request, 'category.html', {'name': id,
-                                            'competencies': competencies} )
+                                             'competencies': competencies})
