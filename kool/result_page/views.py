0b8cedc9a271dd4faa285fd94939a3a34e5f34b7
upsjg <upsjg@student.kit.edu>
2022-12-19 09:12:49 +0000

    [feature] dynamic results via csv

--- kool/result_page/views.py
+++ kool/result_page/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+import pandas as pd
+
+resultsdict = []
+
+csvreader = pd.read_csv('/workspaces/kool/kool/firsttest.csv', 
+                        encoding='UTF-8',
+                        header=None)
+
+# iterating through csv file using pandas
+for expert, trustworthiness in zip(csvreader[0], csvreader[1]):
+    resultsdict.append([expert, trustworthiness])
+
 
 def results(request):
-    return render(request, 'result_page.html')
+    return render(request, 'result_page.html', {'results': resultsdict})
