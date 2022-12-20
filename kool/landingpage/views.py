075c5105e8bf0663e7a011b4e363afcfbd294671
utmyn <utmyn@student.kit.edu>
2022-12-20 14:14:50 +0000

    [feature] searchbar now works

--- kool/landingpage/views.py
+++ kool/landingpage/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-# import pandas as pd
-# from . import models
+import requests
+import json
 
+main_url = "http://192.168.123.116:8020"
 
-# content = []
+def get_request_from_api(url):
+    for i in range(5):
+        try:
+            response = requests.get(url)
+            return response.json()
+        except:
+            time.sleep(2)
+            continue
+    return "connection failed"
+
 
 def home(request):
-    return render(request, 'home.html')
+    req_all_comp = main_url + "/all_competencies/"
+    competencies = get_request_from_api(req_all_comp)
+    print(json.dumps(competencies))
+    return render(request, 'home.html', {'competencies': json.dumps(competencies)})
 
 def aboutkoolpage(request):
     return render(request, 'aboutkool.html')
