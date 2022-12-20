91e82b4f32198c66fe7b865d495ddf7dfab00007
utmyn <utmyn@student.kit.edu>
2022-12-20 09:53:11 +0000

    [feature] added connection to database

--- kool/result_page/models.py
+++ kool/result_page/models.py
@@ -1,7 +1,53 @@
+import requests
 
+main_url = "http://192.168.123.116:8020"
+
+def get_request_from_api(url):
+    for i in range(5):
+        try:
+            response = requests.get(url)
+            return response.json()
+        except:
+            time.sleep(2)
+            continue
+    return "connection failed"
 
 class Result:
 
     id: int
     name: int
     author: dict
+
+class Author:
+    def __init__(self, id: int, first_name: str, last_name: str, abstracts: dict):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.abstracts = {}
+ 
+    def add_abstract(self, abstract_id: int, relevancy: float):
+        endpoint = main_url + "/abstract_by_id/" + str(abstract_id)
+        abstract_db_entry = get_request_from_api(endpoint)
+        abstract = Abstract(abstract_db_entry[0], abstract_db_entry[1],
+                        abstract_db_entry[2], abstract_db_entry[3],
+                        abstract_db_entry[4], abstract_db_entry[5])
+        self.abstracts[abstract_id] = (abstract, relevancy)
+        print(self.abstracts)
+
+    def get_last_publication_year(self):
+        values = list(self.abstracts.values())
+        years = [x[0].year for x in values]
+        return max(years)
+
+    def get_number_of_publications(self):
+        return len(self.abstracts)
+
+class Abstract:
+    
+    def __init__(self, id: int, year: int, title: str, content: str, doctype: str, institution: str):
+        self.id = id
+        self.year = year
+        self.title = title
+        self.content = content
+        self.doctype = doctype
+        self.institution = institution
--- kool/result_page/views.py
+++ kool/result_page/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 import pandas as pd
+import requests
+from result_page.models import Author 
 
 resultsdict = []
 
@@ -7,10 +9,42 @@ csvreader = pd.read_csv('/workspaces/kool/kool/firsttest.csv',
                         encoding='UTF-8',
                         header=None)
 
+main_url = "http://192.168.123.116:8020"
+
 # iterating through csv file using pandas
 for expert, trustworthiness in zip(csvreader[0], csvreader[1]):
     resultsdict.append([expert, trustworthiness])
 
+def get_request_from_api(url):
+    for i in range(5):
+        try:
+            response = requests.get(url)
+            return response.json()
+        except:
+            time.sleep(2)
+            continue
+    return "connection failed"
 
-def results(request, id):
-    return render(request, 'result_page.html', {'results': resultsdict})
+def getAuthorByCompId(competency_id):
+    request_url = main_url + "/authors_by_competency_id/" + str(competency_id)
+    authors_db_entry = get_request_from_api(request_url)
+    authors = {}
+    for author_entry in authors_db_entry:
+        author_id = author_entry[0]
+        author_first_name = author_entry[1]
+        author_last_name = author_entry[2]
+        abstract_id = author_entry[3]
+        relevancy = author_entry[4]
+
+        if author_id not in authors:
+            authors[author_id] = Author(author_id, author_first_name, author_last_name, {})
+    
+        authors[author_id].add_abstract(abstract_id, relevancy)
+    return authors
+
+def results(request, id = None):
+    searchquery = request.GET.get('q', '')
+    print(searchquery)
+    authors = getAuthorByCompId(id)
+    
+    return render(request, 'result_page.html', {'authors': authors})
