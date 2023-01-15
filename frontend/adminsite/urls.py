909b9580f170b2717f2b8c1b56160ecb3a53895a
upsjg <upsjg@student.kit.edu>
2023-01-15 17:17:39 +0100

    [feature] AdminSite scraper, rebuild, add_entries
    Co-authored-by: Furier, Marc Philipp

--- frontend/adminsite/urls.py
+++ frontend/adminsite/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.adminpage, name='adminsite'),
-    path('add_entries/', views.add_entries, name='add_entries')
+    path('add_entries/', views.add_entries, name='add_entries'),
+    path('edit_database/', views.edit_database, name='edit_database'),
+    path('rebuild/', views.rebuild, name='rebuild'),
+    path('scraper/', views.scraper, name='scraper'),
 ]
--- frontend/adminsite/views.py
+++ frontend/adminsite/views.py
@@ -1,9 +1,26 @@
+import pandas as pd
 from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.core.files.storage import FileSystemStorage
 
 
 def adminpage(request):
     return render(request, 'admin.html')
 
-
 def add_entries(request):
+    if request.method == 'POST' and 'new_database' in request.FILES:
+        myfile = request.FILES['new_database']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'admin.html')
     return render(request, 'add_entries.html')
+
+def edit_database(request):
+    return render(request, 'edit_database.html')
+
+def rebuild(request):
+    return render(request, 'rebuild.html')
+
+def scraper(request):
+    return render(request, 'scraper.html')
